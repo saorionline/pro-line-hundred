71930ce8379a7452abce83cbd571bd82adcaf297
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2022-09-01 18:38:59 -0500

    platzi makes me smarter

---
+++ explanation.md
@@ -0,0 +1,9 @@
+¿De que se trata Javascript?
+
+Es un lenguaje interpretado por el navegador. De forma nativa va a poder responder a las instrucciones
+que hangan parte de mi algorítmo. 
+
+¿Qué se puede hacer con Javascript?
+Cargar archivos multimedia, hacer dibujos y animaciones, y hacer programas interactivos. Permite además
+crear sistemas de alto rendimiento, con la robustez suficiendo para que el desarrollador cree su propio 
+código y además, logre escribir un sistema actual y preciso a las necesidades de los usuarios.
---
+++ index.js
@@ -0,0 +1,46 @@
+let a = 3;
+let b = 5;
+
+function suma () {
+
+   sumTwo = a + b;
+   console.log(sumTwo);
+}
+suma();
+
+// second exercise based on 6th grade
+
+let frascosMiel = [10, 18, 20, 30, 40, 60];
+let panales = [3, 5, 6, 9, 10, 16] ;
+
+function proportion () {
+   firstOperation = (20 / 6);
+
+    if (firstOperation == 10/3) {
+        console.log("firstOperations");
+    }
+    else {
+        console.log("next")
+    }
+}
+
+proportion();
+
+// Tercer ejercicio basado en razones equivalentes en el mundo real
+
+let frascosMiel = [10, 18, 20, 30, 40, 60];
+let panales = [3, 5, 6, 9, 10, 16] ;
+
+function proportion () {
+    firstOperation = (20 / 6);
+
+    if (firstOperation == 10/3) {
+        console.log("firstOperations");
+    }
+    else {
+        console.log("next")
+    }
+}
+
+proportion();
+
