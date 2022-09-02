89fc17383ac4ee209bfd0e29393cf28aaa69c22d
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2022-09-01 22:12:58 -0500

    dont make any payment to anyone you dont know

--- index.js
+++ index.js
@@ -15,8 +15,8 @@ let panales = [3, 5, 6, 9, 10, 16] ;
 
 function proportion () {
    firstOperation = (20 / 6);
-
-    if (firstOperation == 10/3) {
+   secondOperation = (30 /9);
+    if (firstOperation == 10/3 && secondOperation == 10/3) {
         console.log("firstOperations");
     }
     else {
@@ -28,19 +28,67 @@ proportion();
 
 // Tercer ejercicio basado en razones equivalentes en el mundo real
 
-let frascosMiel = [10, 18, 20, 30, 40, 60];
-let panales = [3, 5, 6, 9, 10, 16] ;
+let runMinutes = [3, 10, 15, 20, 24, 30];
+let weightLift = [5, 12, 25, 30, 48, 50] ;
 
-function proportion () {
-    firstOperation = (20 / 6);
+function equival () {
+    firstCalcul = (10 / 12);
+    secondCalcul = (15 / 25);
+    thirdCalcul = (20 / 30);
+    fourthCalcul = (24 / 48);
+    fifthCalcul = (30 / 50);
 
-    if (firstOperation == 10/3) {
-        console.log("firstOperations");
+    if (fifthCalcul == 3/5) {
+        console.log("a");
     }
     else {
         console.log("next")
     }
 }
 
-proportion();
+equival();
+
+// Cuarto ejercicio basado en razones equivalentes en el mundo real
+
+let walnuts = [5, 10, 15, 25, 40, 45];
+let candy = [2, 4, 12, 20, 18, 18] ;
+
+function eatingdisor () {
+    firstCalcul = (10 / 4);
+    secondCalcul = (15 / 12);
+    thirdCalcul = (25 / 20);
+    fourthCalcul = (40 / 18);
+    fifthCalcul = (45 / 18);
+
+    if (fifthCalcul == 5/2) {
+        console.log("4.a");
+    }
+    else {
+        console.log("next")
+    }
+}
+
+eatingdisor();
+
+
+// Quinto ejercicio basado en razones equivalentes en el mundo real
+
+let meatPieces = [3, 18, 20, 30, 45, 72];
+let vegetables = [4, 30, 32, 40, 60, 96] ;
+
+function eatingdisor () {
+    firsCalcul = (18 / 30);
+    secondCalcul = (20 / 32);
+    thirdCalcul = (30 / 40);
+    fourthCalcul = (45 / 60);
+    fifthCalcul = (72 / 96);
+
+    if (fifthCalcul == 3/4) {
+        console.log("5");
+    }
+    else {
+        console.log("next")
+    }
+}
+
 
