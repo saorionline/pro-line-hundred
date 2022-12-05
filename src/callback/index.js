eed9ab158f5d2b0f66aea45c4ca88918103d2779
Saori Isabel Tovar Niño <saorisabeltovar@gmail.com>
2022-12-05 11:36:04 -0500

    just a quick but programmed meeting

---
+++ src/callback/index.js
@@ -0,0 +1,19 @@
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+function add(num1, num2, addition) {
+    return addition(num1, num2);
+}
+
+console.log(add(2,2,sum));
+
+setTimeout(function () {
+    console.log('Hi Dear Machine');
+},2000)
+
+function greeting (logicName) {
+    console.log(`How are you ${logicName}`);
+}
+
+setTimeout(greeting, 2000, 'Alexa');
